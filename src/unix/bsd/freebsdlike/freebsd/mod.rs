96220ce805c4997f2cfe07e126f260c69c69ba3a
Brandon Bergren <bdragon@FreeBSD.org>
2021-04-25 17:20:39 -0500

    Add powerpc-unknown-freebsd / powerpcspe-unknown-freebsd support.

--- src/unix/bsd/freebsdlike/freebsd/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/mod.rs
@@ -1569,6 +1569,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "arm")] {
         mod arm;
         pub use self::arm::*;
+    } else if #[cfg(target_arch = "powerpc")] {
+        mod powerpc;
+        pub use self::powerpc::*;
     } else if #[cfg(target_arch = "powerpc64")] {
         mod powerpc64;
         pub use self::powerpc64::*;
